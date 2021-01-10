65c28609bb02108255261d019a217dbad7017966
sameers25 <31129758+sameers25@users.noreply.github.com>
2021-01-10 17:10:04 +0530

    Update databaseConnector.js

--- Nodejs-Express-MongoDb-API/config/databaseConnector.js
+++ Nodejs-Express-MongoDb-API/config/databaseConnector.js
@@ -10,7 +10,7 @@ con.connect(function (err) {
         console.log('Error connecting to Database');
         return;
     } 
-    console.log('Connection established @' + "rpainstance.chqsdddmmhkf.us-east-1.rds.amazonaws.com" );
+   
 });
 
 module.exports = con;
